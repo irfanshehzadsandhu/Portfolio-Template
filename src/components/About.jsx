cd9db90689ce3f09f2d11d82b81259d72d2bc7a8
irfanatcarbonteq <irfan@carbonteq.com>
2024-10-03 03:51:45 +0500

    about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,22 +1,24 @@
-import React from 'react';
-import { motion } from 'framer-motion';
-import { styles } from '../styles';
-import { services } from '../constants';
-import { fadeIn, textVariant } from '../utils/motion';
-import { SectionWrapper } from '../hoc';
+import React from "react";
+import { motion } from "framer-motion";
+import { styles } from "../styles";
+import { services } from "../constants";
+import { fadeIn, textVariant } from "../utils/motion";
+import { SectionWrapper } from "../hoc";
 
 const ServiceCard = ({ index, title, icon }) => {
   return (
     <motion.div
-      variants={fadeIn('right', 'spring', 0.5 * index, 0.75)}
-      className="xs:w-[250px] w-full card-gradient p-[1px] rounded-[20px] shadow-card">
+      variants={fadeIn("right", "spring", 0.5 * index, 0.75)}
+      className="xs:w-[250px] w-full card-gradient p-[1px] rounded-[20px] shadow-card"
+    >
       <div
         options={{
           max: 45,
           scale: 1,
           speed: 450,
         }}
-        className="bg-jetLight rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col">
+        className="bg-jetLight rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col"
+      >
         <img src={icon} alt={title} className="w-16 h-16 object-contain" />
         <h3 className="text-taupe text-[18px] font-bold text-center">
           {title}
@@ -35,14 +37,23 @@ const About = () => {
       </motion.div>
 
       <motion.p
-        variants={fadeIn('', '', 0.1, 1)}
-        className="mt-4 text-taupe text-[18px] max-w-3xl leading-[30px]">
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Debitis
-        sapiente ipsum dolorum dicta eaque cumque inventore molestias, beatae ea
-        quaerat alias accusamus voluptas autem! Alias odit voluptates in totam
-        vitae dignissimos minus eaque culpa unde tempore dolore aperiam
-        obcaecati voluptatum aliquam corrupti, suscipit accusamus! Odit unde
-        veniam dolorum ipsum doloribus.
+        variants={fadeIn("", "", 0.1, 1)}
+        className="mt-4 text-taupe text-[18px] max-w-3xl leading-[30px]"
+      >
+        I possess extensive experience in requirement analysis and software
+        design across multiple projects. My domain expertise encompasses
+        technologies such as Ruby on Rails, Node, React, as well as API
+        integration. I have a proven ability to build APIs tailored for mobile
+        applications and I am proficient in deploying applications on platforms
+        like GCP and AWS. My proficiency spans the entire software development
+        life cycle, from the initial stages of requirement gathering to the
+        final deployment of projects. This holistic understanding allows me to
+        contribute effectively at every phase of development. I am well-versed
+        in agile methodologies, ensuring that projects are not only
+        well-designed but also implemented in a manner that aligns with industry
+        best practices. Throughout my career, I have encountered and
+        successfully navigated various challenges, showcasing my adaptability
+        and problem-solving skills
       </motion.p>
 
       <div className="mt-20 flex flex-wrap gap-10">
@@ -54,4 +65,4 @@ const About = () => {
   );
 };
 
-export default SectionWrapper(About, 'about');
+export default SectionWrapper(About, "about");
