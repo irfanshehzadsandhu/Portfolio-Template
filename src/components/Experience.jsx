de6fafd24d3a8121235a0e4b2aa83826b1dad74f
irfanatcarbonteq <irfan@carbonteq.com>
2024-10-03 05:02:52 +0500

    resume

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -102,7 +102,7 @@ const Experience = () => {
               ease-in-out"
               onClick={() =>
                 window.open(
-                  'resume link', //paste the link to your resume here
+                  'https://drive.google.com/file/d/1iIQ3M7XyeT5o0LatG_1XGjWwAH7jyCQM/view?usp=sharing', //paste the link to your resume here
                   '_blank'
                 )
               }
